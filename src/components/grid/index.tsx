db326824144dca5cc949dfc8b224f05a5bbaeb32
sergeykasyan <s.kasyan@list.ru>
2020-05-21 21:45:23 +0300

    Add scanner

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -17,10 +17,14 @@ function timeout(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+const CELL_SIZE = 25;
+
 export const Grid = () => {
   const dispatch = useDispatch();
   const { columns, rows, running } = useSelector((state: RootState) => state.app);
   const [field, setField] = useState<FieldType>([]);
+  const [ isHorizontal, setHorizontal ] = useState(true);
+  const [ scannerPosition, setScannerPosition ] = useState(0);
 
   useEffect(() => {
     if (!columns || !rows) {
@@ -33,55 +37,68 @@ export const Grid = () => {
   }, [columns, rows]);
 
   useEffect(() => {
-   const solve = async () => {
-     if (running) {
-       try {
-         // const resolved = resolveNonogram(rows, columns);
-         // setField(resolved);
+    const solve = async () => {
+      if (running) {
+        try {
+          let hasChanges;
 
-         let hasChanges;
+          do {
+            hasChanges = false;
 
-         do {
-           hasChanges = false;
+            // Go through rows
+            setHorizontal(true);
+            for (let rowNumber = 0; rowNumber < rows.length; rowNumber++) {
+              setScannerPosition(rowNumber);
+              const row = field[rowNumber];
+              const rowDefinition = rows[rowNumber];
+              const result = tryResolveRow(row, rowDefinition);
+              if (!isArraysEqual(row, result)) {
+                hasChanges = true;
+              }
+              field.splice(rowNumber, 1, result);
 
-           // Go through rows
-           for (let rowNumber = 0; rowNumber < rows.length; rowNumber++) {
-             const row = field[rowNumber];
-             const rowDefinition = rows[rowNumber];
-             const result = tryResolveRow(row, rowDefinition);
-             if (!isArraysEqual(row, result)) {
-               hasChanges = true;
-             }
-             field.splice(rowNumber, 1, result);
+              await timeout(30);
+              setField(field.slice());
+            }
 
-             await timeout(30);
-             setField(field.slice());
-           }
-
-           // Go through columns
-           for (let colNumber = 0; colNumber < columns.length; colNumber++) {
-             const row = createRowFromColumn(field, colNumber);
-             const colDefinition = columns[colNumber];
-             const result = tryResolveRow(row, colDefinition);
-             if (!isArraysEqual(row, result)) {
-               hasChanges = true;
-             }
-             applyColumnResult(field, colNumber, result);
-             await timeout(30);
-             setField(field.slice());
-           }
-         } while (hasChanges);
+            // Go through columns
+            setHorizontal(false);
+            for (let colNumber = 0; colNumber < columns.length; colNumber++) {
+              setScannerPosition(colNumber);
+              const row = createRowFromColumn(field, colNumber);
+              const colDefinition = columns[colNumber];
+              const result = tryResolveRow(row, colDefinition);
+              if (!isArraysEqual(row, result)) {
+                hasChanges = true;
+              }
+              applyColumnResult(field, colNumber, result);
+              await timeout(30);
+              setField(field.slice());
+            }
+          } while (hasChanges);
 
 
-         dispatch(AppActions.setRunning(false));
-       } catch (e) {
-         console.log('Error', e);
-       }
-     }
-   };
+          dispatch(AppActions.setRunning(false));
+        } catch (e) {
+          console.log('Error', e);
+        }
+      }
+    };
 
-   solve();
+    solve();
   }, [columns, rows, running]);
+
+  const scannerStyle = {
+    ...(isHorizontal ? {
+      height: CELL_SIZE,
+      width: columns.length * CELL_SIZE,
+      top: scannerPosition * CELL_SIZE,
+    } : {
+      width: CELL_SIZE,
+      height: rows.length * CELL_SIZE,
+      left: scannerPosition * CELL_SIZE,
+    }),
+  };
 
   return (
     <table className={styles.table}>
@@ -101,13 +118,18 @@ export const Grid = () => {
               }
               return (
                 <td key={colIndex} className={classes.join(' ')}>
-                  {col === -1 && <div className={styles.dotWrapper}><div className={styles.dot}/></div>}
+                  {col === -1 && <div className={styles.dotWrapper}>
+                    <div className={styles.dot}/>
+                  </div>}
                 </td>
               );
             })}
           </tr>
         );
       })}
+      {running &&
+      <tr className={styles.scanner} style={scannerStyle}/>
+      }
       </tbody>
     </table>
   );
