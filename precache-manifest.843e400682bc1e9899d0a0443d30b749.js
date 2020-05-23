b9445be6bc6557ed3f04f2e9585189978397b1d3
sergeykasyan <s.kasyan@list.ru>
2020-05-23 15:01:38 +0300

    Updates

---
+++ precache-manifest.843e400682bc1e9899d0a0443d30b749.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca3a3fe827aa2d145f5bfff3076e5e86",
+    "url": "/nonogram-app/index.html"
+  },
+  {
+    "revision": "88881e3baa726d10d661",
+    "url": "/nonogram-app/static/css/main.30361895.chunk.css"
+  },
+  {
+    "revision": "0c43e91b306b035042f5",
+    "url": "/nonogram-app/static/js/2.48b7a40a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nonogram-app/static/js/2.48b7a40a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88881e3baa726d10d661",
+    "url": "/nonogram-app/static/js/main.508ac1c4.chunk.js"
+  },
+  {
+    "revision": "280f7df5b9df6b4ca78e",
+    "url": "/nonogram-app/static/js/runtime-main.7de2b802.js"
+  },
+  {
+    "revision": "cb42b4c5d4c76e8a712ac61f42bcc4ed",
+    "url": "/nonogram-app/static/media/expand_more.cb42b4c5.svg"
+  }
+]);
